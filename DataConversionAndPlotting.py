77f34ebe928df7248468f2c7c4ed79c595c873c9
BioCSK <71597530+BioCSK@users.noreply.github.com>
2020-10-08 09:19:25 +0900

    Update DataConversionAndPlotting.py

--- DataConversionAndPlotting.py
+++ DataConversionAndPlotting.py
@@ -1,24 +1,27 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib as mpl
+
 
 class DataConversionAndPlotting():
     """
-    Date:2020.10.07
+    Date:2020.10.05 (updated 10.08)
     Purpose: Data conversion  & plotting 
     Writer: kcs / katd6@naver.com
     """
-    def __init__(self,dataFile,skipRows,nRows,useCols,tumorType):
+    def __init__(self,dataFile,skipRows,nRows,useCols,sheet_name,tumorType):
         """
         skiprows= 생략할 행 수 
         nRows=mouse n 수
         useCols= tumour Volume 측정일수 
         tumorType= 암종 
+
         """
 
-        self.data=pd.read_excel(dataFile,skiprows=skipRows,nrows=nRows,usecols=useCols)
+        self.data=pd.read_excel(dataFile,skiprows=skipRows,nrows=nRows,usecols=useCols,sheet_name=sheet_name)
         self._tumorType=tumorType
-
+        print(self.data.head(3))
     def dataConvert(self):
 
         y=pd.concat([self.data.iloc[:,[0]],self.data.iloc[:,[1]]],axis=1)
@@ -49,32 +52,33 @@ class DataConversionAndPlotting():
                 temp = i
                 index_of_group.append(j-1)
         index_of_group.append(len(list(self.data.iloc[:,0])))
-        print(" plotting할 총 실험 그룹 개수는 [{}]개 입니다. ".format(len(index_of_group)))
+        print(" 실험 그룹은 {}이며, plotting할 총 실험 그룹 개수는 [{}]개 입니다. ".format(self.data.iloc[:,0],len(index_of_group)))
         ## 각 그룹별로 statistic information 확인
         temp=0
         mean_df=pd.DataFrame()
         std_df=pd.DataFrame()
         for i in sorted(index_of_group):
             mean_df=pd.concat([self.data.iloc[temp:i,].describe().iloc[[1],:],mean_df])
-            std_df=pd.concat([self.data.iloc[temp:i,].describe().iloc[[2],:],std_df]) ## 그룹별 mean, std 값 추출완료 
+            std_df=pd.concat([self.data.iloc[temp:i,].describe().iloc[[2],:],std_df]) ## 그룹별 mean, std 값 추출완료
             temp=i
+        print(index_of_group)
         ## naming group
+       
         mean_df.index=list(self.data.iloc[:,0].cat.categories)
         std_df.index=list(self.data.iloc[:,0].cat.categories)
         ## ploting 
+        cmap=plt.get_cmap('jet')
+        colors=[cmap(i) for i in np.linspace(0,1,len(mean_df.index))]
         for i in range(len(mean_df.index)):
             y=pd.Series(mean_df.iloc[i,:].astype('float64'))
             x=pd.Series(mean_df.columns).astype("int64")
             x.index=range(1,len(x)+1)
             ymask=np.isfinite(y)
-            plt.plot(x[ymask],y[ymask], linestyle='-', marker='o',label='{}'.format(mean_df.index[i-1]))
-    
-        plt.title("{} Tumor type ".format(self._tumorType))
+            plt.plot(x[ymask],y[ymask], linestyle='-', marker='o',label='{}'.format(mean_df.index[i]),color=colors[i])
+        
+        plt.title("{} ".format(self._tumorType))
         plt.ylabel("Tumor volume (mm^3)")
         plt.xlabel("Days after inoculation")
         plt.legend()
         plt.show()
-
-
-
 
