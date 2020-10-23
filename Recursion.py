4727caff64659138d52448a19fcaa3ad12febc90
BioCSK <71597530+BioCSK@users.noreply.github.com>
2020-10-23 11:48:04 +0900

    Update Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -1,24 +1,19 @@
 
 
-def RecursionSolve(encoding,result,count):
-    if isinstance(encoding,list):
-        for i in encoding:
-            if i>90:
-                global t
-                t=(i-90)+64
-                if t <= 90:
-                    result.append("A"+chr(t))
-                else:
-                    RecursionSolve(t,result,count)
-            else:result.append(chr(t))
-    else:
-        if encoding > 90:
-            count+=1
-            encoding=(encoding-90)+64
-            if encoding <= 90:
-                result.append(chr(65+count)+chr(encoding))
-            else:
-                RecursionSolve(encoding,result,count)
+
+def RecursionSolve(encoding):
+    count=0
+    temp=[]
+    for i in encoding:
+        if i <=90:
+            temp.append(chr(i))
         else:
-            result.append
-            return result
+            while (i > 90):
+            
+                count+=1
+                i=i-90+64
+                if i <= 90:
+                    temp.append(chr(64+count)+chr(i))
+                    count=0
+    return temp
+
