5a2b6817854d48d48474773f603fc0429e7a6265
BioCSK <71597530+BioCSK@users.noreply.github.com>
2021-02-08 14:15:43 +0900

    Update prepCode.R

--- prepCode.R
+++ prepCode.R
@@ -1,7 +1,91 @@
+
+
+
+\documentclass[a4paper, 11pt]{report}
+\usepackage[left=1.5cm,right=1.5cm,top=2cm,bottom=4.5cm,a4paper]{geometry}
+\usepackage[utf8]{inputenc}
+\usepackage{amsmath}
+\usepackage{amssymb}
+\usepackage{booktabs}
+\usepackage{kotex}
+\usepackage[hangul, nonfrench]{dhucs}
+\usepackage{titlesec}
+
+\renewcommand{\arraystretch}{1.2}
+
+\def\thesection{\arabic{section}}
+\def\thesubsection{\arabic{section}.\arabic{subsection}}
+\def\thesubsubsection{\arabic{section}.\arabic{subsection}.\arabic{subsubsection}}
+
+\titleformat{\paragraph}
+{\normalfont\normalsize\bfseries}{\theparagraph}{1em}{}
+\titlespacing*{\paragraph}
+{0pt}{3.25ex plus 1ex minus .2ex}{1.5ex plus .2ex}
+
+\usepackage{fancyhdr}
+\pagestyle{fancy}\setlength\headheight{100pt}
+\fancyhead[L]{\includegraphics[width=4cm]{C://Users//user//Desktop//APEX//Logo//APEX_logo.png}}
+\fancyhead[R]{\textbf{Center for Advancing Cancer Therapeutics\\}}
+
+\renewcommand{\headrulewidth}{2pt}
+\renewcommand{\footrulewidth}{1pt}
+
+\usepackage{longtable}
+\usepackage{caption}
+\usepackage{indentfirst}
+%\parindent=1em
+ \usepackage{subcaption}
+\setcounter{secnumdepth}{4}
+\setcounter{tocdepth}{4}
+
+\renewcommand{\contentsname}{TABLE OF CONTENTS}
+%\newenvironment{knitrout}{}{} 
+\renewcommand{\contentsname}{목록}
+
+\usepackage{alltt}
+\usepackage{booktabs}
+\usepackage{multirow}
+\usepackage{makecell}
+\usepackage{setspace}
+\usepackage{type1cm}
+\usepackage{array}
+\usepackage{pdflscape, lipsum}
+\usepackage{enumerate}
+
+\usepackage{graphicx}
+\DeclareGraphicsExtensions{.pdf,.png,.jpg}
+\usepackage[table,xcdraw]{xcolor}
+\usepackage{floatrow}
+
+\linespread{1.5}
+\IfFileExists{upquote.sty}{\usepackage{upquote}}{}
+
+\begin{document}
+\addtocounter{section}{5}
+
+\section{결과 및 토의}
+
+<<echo=FALSE, warning=FALSE, error=FALSE, results='asis', message=F >>=
+Round = function(x, n=0)
+{
+  posneg = sign(x)
+  z = abs(x)*10^n
+  z = z + 0.5
+  z = trunc(z)
+  z = z/10^n
+  return(z*posneg)
+}
+getTGIPercent = function(drugName,time=timeDay,controlName="vehicle"){
+  deltaControl = mean(totalData$tumor_Volume[totalData$group==controlName & totalData$Time_Day==time]) - mean(totalData$tumor_Volume[totalData$group==controlName & totalData$Time_Day==0])
+  deltaDrug = mean(totalData$tumor_Volume[totalData$group==drugName & totalData$Time_Day==time])  - mean(totalData$tumor_Volume[totalData$group== drugName & totalData$Time_Day== 0 ])
+  return(Round((1-deltaDrug/deltaControl)*100,2))
+}
 prepCode<-function(fileName,sheetName,target="tumor"){
+  library(stringr)
   library(readxl)
   library(dplyr)
-testcase=read_excel(fileName,sheetName)
+  library(stringi)
+  testcase=read_excel(fileName,sheetName)
 range=vector()
 newDataframe=data.frame()
 roof_break=0
@@ -49,10 +133,11 @@ while(TRUE){
   }
   start_row_index=start_row_index+1 
 }
-cat("?????????????????? ?????? ?????????  :" ,time , "?????? ?????? ?????? :" , "(",start_row_index ,",",start_col_index ,")", "????????? ????????? ??????: " ,range ,"??? ?????????????" )
+##cat("?????????????????? ?????? ?????????  :" ,time , "?????? ?????? ?????? :" , "(",start_row_index ,",",start_col_index ,")", "????????? ????????? ??????: " ,range ,"??? ?????????????" )
 edit(testcase)
 
-ans <-readline('insert plseas : ')
+##ans <-readline('insert plseas : ')
+ans <-"yes"
 if(ans == "yes"){
   if(target == "tumor"){
   for( j in 1:length(repeated)){ 
@@ -71,7 +156,7 @@ else if(target=="weight"){
 repeated=seq(start_col_index,ncol(testcase),6)
   for( j in 1:length(repeated)){ 
     for(i in 1:(length(range)-1)){
-      tempDf=data.frame(testcase[range[i]:(range[i+1]-1),repeated[j]+2],testcase[time,repeated[j]],testcase[range[i]:(range[i+1]-1),repeated[j]+3],testcase[range[i],repeated[j]+1])  ## ????????? df??? V 
+      tempDf=data.frame(testcase[range[i]:(range[i+1]-1),repeated[j]+2],testcase[time,repeated[j]],testcase[range[i]:(range[i+1]-1),repeated[j]+3],testcase[range[i],repeated[j]+1])  ## ????????? df??? 
       colnames(tempDf)=c("ID","Time_Day","Weight","Treatment")
       newDataframe=rbind(newDataframe,tempDf)
     }
@@ -88,11 +173,70 @@ return(newDataframe)
     stop("원본 데이터 재확인해주세요. ")
   }
 }
-
-getwd()
 setwd("/Users/user/Desktop/RR_pilot/")
-tumorSize=prepCode("(----.xlsx","효능평가 종양측정 및 종양 성장 그래프","tumor")
-weight= prepCode("---.xlsx","체중 측정 값 및 체중 변화 그래프",target = "weight")
-tumorSize$TumorVolume=as.numeric(tumorSize$Long_mm)*(as.numeric(tumorSize$Short_mm)/2)**2
-totalData=data.frame(tumorSize[,1:4],tumorSize$TumorVolume,weight$Weight,weight$Treatment)
+  tumorSize=prepCode("-----------.xlsx","효능평가 종양측정 및 종양 성장 그래프","tumor")
+  weight= prepCode("------.xlsx","체중 측정 값 및 체중 변화 그래프",target = "weight")
+groupSeparation = read_excel("GroupSeparation.xlsx")
+tumorSize$TumorVolume=as.numeric(tumorSize$Long_mm)*((as.numeric(tumorSize$Short_mm)/2)**2)*2
+totalData=data.frame(tumorSize[,1],as.numeric(tumorSize$Time_Day),tumorSize[,3:4],tumorSize$TumorVolume,as.numeric(weight$Weight),weight$Treatment)
+colnames(totalData)=c("ID","Time_Day","Long_mm^3","Short_mm^3","tumor_Volume","weight","group")
+totalData$group = str_replace_all(totalData$group,"[\r\n]"," ")
+totalData$group = stri_trim(totalData$group)
+getAvgAndSd= function(groupSeparation){
+  temp = groupSeparation %>% group_by(Group) %>% summarise(Average=mean(tumorVolume)) %>% select(Group,tumorVolume=Average)
+  temp$ID="Mean"
+  temp2 = groupSeparation %>% group_by(Group) %>% summarise(SD=sd(tumorVolume)) %>% select(Group,tumorVolume=SD)
+  temp2$ID="SD"
+  tempDf = rbind(temp,temp2) %>% select(ID,tumorVolume,Group) %>% rbind(groupSeparation) %>% arrange(ID)
+  return(tempDf)
+}
+library(dplyr)
+library(ggplot2)
+library(knitr)
+library(kableExtra)
+library(xtable)
+ 
+@
+
+\subsubsection{종양 성장 확인 및 군 분리}
+\noindent - ----- 일째, ---- \Sexpr{Round(mean(groupSeparation$tumorVolume),2)} $mm^3$ 오차 $\pm$ \Sexpr{Round(sd(groupSeparation$tumorVolume),2)} -----------
+\newline
+<<echo=FALSE, warning=FALSE, error=FALSE, message=F >>=
+
+groupSeparation = getAvgAndSd(groupSeparation = groupSeparation)
+groupSeparation$tumorVolume = Round(groupSeparation$tumorVolume,2)
+tempList=list()
+groupingColume=2
+group=unique(groupSeparation$Group)
+  for(i in seq_along(group)){
+   tempList[[i]] = groupSeparation%>% filter(Group == group[i]) %>% select(ID,tumorVolume) 
+  }
+tempList=do.call(cbind, tempList)
+kable(tempList,format="latex", booktabs=T, align="c" , caption="군 분리 시 종양 부피")%>% kable_styling(font_size = 12,latex_options =c("hold_position")) %>% add_header_above(c(
+    setNames(groupingColume,group[1]),setNames(groupingColume,group[2]),setNames(groupingColume,group[3]),setNames(groupingColume,group[4])
+    ### 군분리 group별로 header를 추가합니다. 
+  ),bold = T, italic = T)
+@
+
+
+\noindent - ---- \Sexpr{getTGIPercent("---  15mg/kg",35)}\% 
+\newline
+\newline
+\noindent  * TGI: Tumor growth inhibition \{1-($\Delta$T/$\Delta$C)\}$\times100$
+\newline
+<<echo=FALSE, warning=FALSE, error=FALSE, results='asis', message=F>>=
+tumorTable=totalData %>% select(ID,Time_Day,`Long_mm^3`,`Short_mm^3`,tumor_Volume,group)
+for(i in 3:5){
+  tumorTable[,i]=Round(as.numeric(tumorTable[,i]),2)
+}
+colnames(tumorTable)=c("ID","Time","Long(mm)","Short(mm)","Size(mm^3)","Group")
+kable(
+  x=tumorTable
+                ,format="latex", longtable=T, booktabs=T, align="c", caption="종양부피의 평균 및 표준편차") %>% kable_styling(font_size = 12,latex_options =c("hold_position")) 
+@
+
+
+
+
+
 
